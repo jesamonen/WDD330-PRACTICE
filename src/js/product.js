5a29a41669ddcc7d6836d5a30271059a05d043ba
jesamonen <jesamonen01@gmail.com>
2026-09-18 21:33:11 +0100

    commit

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,29 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // 1. Get existing cart contents or initialize an empty array if empty/null
+  let cart = getLocalStorage("so-cart");
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // 2. Use push to add the new product to the array
+  cart.push(product);
+
+  // 3. Save updated array back to localStorage
+  setLocalStorage("so-cart", cart);
 }
-// add to cart button event handler
+
+// Add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+// Add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
